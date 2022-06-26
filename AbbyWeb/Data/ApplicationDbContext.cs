7f796be9d9adc62b32f866829cf85cbd4fa8e036
ladduumaammu <umajangam97@gmail.com>
2022-06-26 15:05:48 -0500

    My first Commit

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext : DbContext //it is in EntityFrameworkcore namespace
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //to work for the connection string. it is a constructor. passing to the base class of dbcontext
+        {
+
+        }
+        public DbSet<Category> Category { get;set;} //Name of the datatable in the database
+    }
+}
---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]        //Explicitly defining a primary key
+        public int Id { get; set; }
+        [Required]      //It is a required property
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
